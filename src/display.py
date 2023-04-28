d1324e1da735788c09c96e231250195d77f34cf2
hieutranbu <hieutran04901@gmail.com>
2023-04-28 05:12:34 -0400

    Update playable menu

--- src/display.py
+++ src/display.py
@@ -7,6 +7,7 @@ from fgame.question_frame import *
 from fgame.response_frame import *
 from fgame.answer_frame import *
 from fgame.info_frame import *
+from fgame.menu_frame import *
 from ult.buttons import *
 
 # Init game resolution
@@ -25,7 +26,7 @@ read_question = pd.read_csv('./data/Q_ID.csv')
 read_question = read_question.set_index('ID')
 
 seed = 0
-frames = [0, 1, 0, 0, 0, 0]
+frames = [1, 0, 0, 0, 0, 0]
 '''
 0: Menu
 1: Question
@@ -39,12 +40,13 @@ fquest_frame = Q_frame(screen, Xscreen, Yscreen, 'Choose a question')
 fresponse_frame = Response_frame(screen, Xscreen, Yscreen)
 fanswer_frame = Answer_frame(screen, Xscreen, Yscreen)
 finfo_frame = Info_frame(screen, Xscreen, Yscreen)
+fmenu_frame = Menu_frame(screen, Xscreen, Yscreen)
 
 frame_cnt_thinking = 50
 frame_cnt_response = 50
 frame_cnt_answer = 300
-
 frame_cnt_random = 30
+text_score = 440
 
 shuffle_question = True
 shuffle_response = True
@@ -56,23 +58,12 @@ reset_arrow = False
 
 interact_1, interact_2, interact_3 = None, None, None
 arrow, arrow_rect = None, None
+answer_box_rect = None
+home_rect, restart_rect = None, None
 
 while running:
     # White background color
     screen.fill((255, 255, 255))
-
-    # if frames[1] == 1 or frames[2] == 1 or frames[3] == 1:
-    #     character = pygame.image.load('./sprites/character/random.png')
-    #     character = pygame.transform.scale_by(character, 0.1)
-    #     if init_random:
-    #         xrandom = random.randint(0, Xscreen)
-    #         yrandom = random.randint(0, Yscreen)
-    #         init_random = False
-    #     screen.blit(character, (xrandom, yrandom)) 
-    #     frame_cnt_random -= 1
-    #     if frame_cnt_random < 0:
-    #         init_random = True
-    #         frame_cnt_random = 30
         
     if init_params == True:
         # Init Org choice and answer
@@ -90,13 +81,24 @@ while running:
         # Question
         question = df.columns[4:].to_list()
         game_question = df.columns[4:].to_list()
+        shuffle_question = True
         init_params = False
         
     # May reshuffle the questions
     if shuffle_question == True:
         random.shuffle(game_question)
         shuffle_question = False
-
+    
+    # Menu frame
+    if frames[0] == 1:
+        # display_depauw(screen, Xscreen//2, Yscreen//2-180)
+        startRect, tutorialRect = fmenu_frame.display_menu(48, False, False)
+        mpos = pygame.mouse.get_pos()
+        if startRect.collidepoint(mpos):
+            startRect, tutorialRect = fmenu_frame.display_menu(48, True, False)
+        if tutorialRect.collidepoint(mpos):
+            startRect, tutorialRect = fmenu_frame.display_menu(48, False, True)
+        
     # Question frame
     if frames[1] == 1:
         if len(game_question) > 2:
@@ -112,8 +114,9 @@ while running:
         
         # Colorful Animation
         mpos = pygame.mouse.get_pos()
-        
+        restart, restart_rect = display_restart(Xscreen//2, 50)
         home, home_rect = display_home(Xscreen//2-470, Yscreen//2+280)
+        screen.blit(restart, restart_rect)
         screen.blit(home, home_rect)
         answer_box, answer_box_rect = display_checkbox(
             screen,
@@ -123,6 +126,9 @@ while running:
         if home_rect.collidepoint(mpos):
             fill(home, pygame.Color(0, 0, 255))
             screen.blit(home, home_rect)
+        if restart_rect.collidepoint(mpos):
+            fill(restart, pygame.Color(255, 0, 0))
+            screen.blit(restart, restart_rect)
         if (interact_1 != None) and (interact_2 != None):
             if interact_1.collidepoint(mpos): 
                 interact_1, interact_2 = fquest_frame.display(
@@ -244,19 +250,42 @@ while running:
             if arrow_rect.collidepoint(mpos):
                 fill(arrow, pygame.Color(0, 0, 255))
                 screen.blit(arrow, arrow_rect)
-            
-            # home, home_rect = display_home(Xscreen, Yscreen)
-            # screen.blit(home, home_rect)
-            # if home_rect.collidepoint(mpos):
-            #     fill(home, pygame.Color(255, 0, 0))
-            #     screen.blit(home, home_rect)
-                
+        score_display, scoreRect = text_box(
+            'Comicsansms',
+            f'Total Score: {text_score}',
+            48,
+            Xscreen//2,
+            20,
+            (0,0,0)
+        )
+        screen.blit(score_display, scoreRect)
+
+    if (frames[1] == 1) or (frames[2] == 1) or (frames[3] == 1):
+        add_deco(screen, './sprites/deco/deco_1.png', 1, 100, 150) 
+        add_deco(screen, './sprites/deco/deco_2.png', 0.3, 920, 315)      
+        add_deco(screen, './sprites/deco/deco_3.jpg', 0.15, 600, 608)
+        add_deco(screen, './sprites/deco/deco_4.jpg', 0.13, 800, 608)
+        score_display, scoreRect = text_box(
+            'Comicsansms',
+            f'Score: {text_score}',
+            32,
+            100,
+            20,
+            (0,0,0)
+        )
+        screen.blit(score_display, scoreRect)
     # Play action
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]: 
+                # Menu frame
+                if (frames[0] == 1):
+                    mpos = pygame.mouse.get_pos()
+                    if startRect.collidepoint(mpos):
+                        frames[0], frames[1] = 0, 1
+
                 # Question frame
                 if (frames[1] == 1):
                     if (interact_1 != None) and (interact_2 != None):
@@ -267,6 +296,7 @@ while running:
                             game_question.remove(game_question[0])
                             shuffle_question = True
                             del_question = True
+                            text_score -= 20
 
                         if interact_2.collidepoint(mpos):
                             frames[1], frames[2] = 0, 1
@@ -274,12 +304,23 @@ while running:
                             game_question.remove(game_question[1])
                             shuffle_question = True
                             del_question = True
+                            text_score -= 20
 
                     if answer_box_rect != None:   
                         if answer_box_rect.collidepoint(mpos):
                             del_question = True
                             frames[4], frames[1] = 1, 0
-                                                
+                    
+                    if home_rect != None:
+                        if home_rect.collidepoint(mpos): 
+                            frames[1], frames[0] = 0, 1
+                            init_params = True 
+                    
+                    if restart_rect != None:
+                        if restart_rect.collidepoint(mpos):
+                            init_params = True
+                            text_score = 220
+
                 # Answer frame
                 if (frames[4] == 1):
                     if (interact_1 != None) and (interact_2 != None) and (interact_3 != None):
@@ -300,6 +341,7 @@ while running:
                     if arrow_rect.collidepoint(mpos):
                         frames[5], frames[1] = 0, 1
                         init_params = True
+                
 
             if reset_arrow == True:
                 arrow = None
@@ -311,6 +353,8 @@ while running:
                 interact_2 = None
                 interact_3 = None
                 atextRect = None
+                home_rect = None
+                restart_rect = None
                 del_question = False
                 
     # print(curr_org)        
--- src/fgame/menu_frame.py
+++ src/fgame/menu_frame.py
@@ -1,3 +1,166 @@
 import pygame
+from ult.text_display import *
+from ult.buttons import *
 
-class ():
+class Menu_frame():
+    def __init__(self, screen, Xscreen, Yscreen):
+        self.screen = screen
+        self.Xscreen = Xscreen
+        self.Yscreen = Yscreen
+    
+    def add_characters(self):
+        character = pygame.image.load('./sprites/character/Ask_Me.jpg')
+        # Scale the image to your needed size
+        character = pygame.transform.scale_by(character, 0.63)
+        self.screen.blit(character, (-60, 300)) 
+    
+    def add_characters_2(self):
+        character = pygame.image.load('./sprites/character/flip.png')
+        # Scale the image to your needed size
+        character = pygame.transform.scale_by(character, 0.63)
+        self.screen.blit(character, (730, 300)) 
+    
+    def add_deco_1(self):
+        character = pygame.image.load('./sprites/deco/deco_menu_1.jpg')
+        # Scale the image to your needed size
+        character = pygame.transform.scale_by(character, 0.8)
+        character_rect = character.get_rect()
+        character_rect.center = (150, 130)
+        self.screen.blit(character, character_rect)
+    
+    def add_deco_2(self):
+        character = pygame.image.load('./sprites/deco/deco_menu_2.png')
+        # Scale the image to your needed size
+        character = pygame.transform.scale_by(character, 0.3)
+        character_rect = character.get_rect()
+        character_rect.center = (800, 150)
+        self.screen.blit(character, character_rect) 
+       
+    def display_menu(self, size, hoover_start, hoover_tutorial):
+        # Start box
+        if hoover_start:
+            start, startRect = text_box(
+                './font/Atma-Bold.ttf', 
+                'START', 
+                size+20, 
+                self.Xscreen//2, 
+                self.Yscreen//2+100, 
+                (0, 0, 0), 
+                custom_font=True
+                # bold=True
+            )
+            display_animationbox(
+                self.screen,
+                startRect,
+                30, 50, 405+20, 105+20
+            )
+            startRect_interact_1 = display_animationbox(
+                self.screen,
+                startRect,
+                30, 50, 400+20, 100+20
+            )
+        else:
+            start, startRect = text_box(
+                './font/Atma-Bold.ttf', 
+                'START', 
+                size, 
+                self.Xscreen//2, 
+                self.Yscreen//2+100, 
+                (0, 0, 0), 
+                custom_font=True
+                # bold=True
+            )
+            display_animationbox(
+                self.screen,
+                startRect,
+                30, 50, 405, 105
+            )
+            startRect_interact_1 = display_animationbox(
+                self.screen,
+                startRect,
+                30, 50, 400, 100
+            )
+        
+        pygame.draw.rect(self.screen, (255,245,0), startRect_interact_1, 0, 10)
+        self.screen.blit(start, startRect)
+
+        if hoover_tutorial:
+            tutorial, tutorialRect = text_box(
+                './font/Atma-Bold.ttf', 
+                'TUTORIAL', 
+                size+20, 
+                self.Xscreen//2, 
+                self.Yscreen//2+250, 
+                (255, 255, 255), 
+                custom_font=True
+                # bold=True
+            )
+            tutorialRect_shadow = display_animationbox(
+                self.screen,
+                tutorialRect,
+                30, 50, 405+20, 105+20
+            )
+            tutorialRect_interact_1 = display_animationbox(
+                self.screen,
+                tutorialRect,
+                30, 50, 400+20, 100+20
+            )
+        else:
+            tutorial, tutorialRect = text_box(
+                './font/Atma-Bold.ttf', 
+                'TUTORIAL', 
+                size, 
+                self.Xscreen//2, 
+                self.Yscreen//2+250, 
+                (255, 255, 255), 
+                custom_font=True
+                # bold=True
+            )
+            tutorialRect_shadow = display_animationbox(
+                self.screen,
+                tutorialRect,
+                30, 50, 405, 105
+            )
+            tutorialRect_interact_1 = display_animationbox(
+                self.screen,
+                tutorialRect,
+                30, 50, 400, 100
+            )
+
+        pygame.draw.rect(self.screen, (255,245,0), tutorialRect_shadow, 3, 10)
+        pygame.draw.rect(self.screen, (0,0,0), tutorialRect_interact_1, 0, 10)
+        self.screen.blit(tutorial, tutorialRect)
+
+        # Title
+        deboo, debooRect = text_box(
+            './font/Atma-Bold.ttf', 
+            'DeBOO', 
+            48+50, 
+            self.Xscreen//2, 
+            self.Yscreen//2-100, 
+            (255,245,0), 
+            custom_font=True
+            # bold=True
+        )
+        deboo_shadow = display_animationbox(
+            self.screen,
+            debooRect,
+            30, 50, 405, 155
+        )
+        deBoo_interact = display_animationbox(
+            self.screen,
+            debooRect,
+            30, 50, 400, 150
+        )
+        pygame.draw.rect(self.screen, (255, 254, 0), deboo_shadow, 3, 10)
+        pygame.draw.rect(self.screen, (0,0,0), deBoo_interact, 0, 10)
+        self.screen.blit(deboo, debooRect)
+
+        self.add_characters()
+        self.add_characters_2()
+        self.add_deco_1()
+        self.add_deco_2()
+
+        return startRect, tutorialRect
+
+

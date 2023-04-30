299a13a1ec0a0dd03fe772c80e5b854265670415
hieutranbu <hieutran04901@gmail.com>
2023-04-30 16:21:56 -0400

    Clean menu frame

--- src/fgame/menu_frame.py
+++ src/fgame/menu_frame.py
@@ -8,37 +8,61 @@ class Menu_frame():
         self.screen = screen
         self.Xscreen = Xscreen
         self.Yscreen = Yscreen
-    
-    def add_characters(self):
-        character = pygame.image.load('./sprites/character/Ask_Me.jpg')
-        # Scale the image to your needed size
-        character = pygame.transform.scale_by(character, 0.63)
-        self.screen.blit(character, (-60, 300)) 
-    
-    def add_characters_2(self):
-        character = pygame.image.load('./sprites/character/flip.png')
-        # Scale the image to your needed size
-        character = pygame.transform.scale_by(character, 0.63)
-        self.screen.blit(character, (730, 300)) 
-    
-    def add_deco_1(self):
-        character = pygame.image.load('./sprites/deco/deco_menu_1.jpg')
-        # Scale the image to your needed size
-        character = pygame.transform.scale_by(character, 0.8)
-        character_rect = character.get_rect()
-        character_rect.center = (150, 130)
-        self.screen.blit(character, character_rect)
-    
-    def add_deco_2(self):
-        character = pygame.image.load('./sprites/deco/deco_menu_2.png')
-        # Scale the image to your needed size
-        character = pygame.transform.scale_by(character, 0.3)
-        character_rect = character.get_rect()
-        character_rect.center = (800, 150)
-        self.screen.blit(character, character_rect) 
-       
+
+    def display_deco(self):
+        display_element(
+            self.screen,
+            './sprites/character/Ask_Me.jpg',
+            110, 
+            500,
+            0.63
+        )
+        display_element(
+            self.screen,
+            './sprites/character/flip.png',
+            910,
+            500,
+            0.63
+        )
+        display_element(
+            self.screen,
+            './sprites/deco/deco_menu_1.jpg',
+            150, 
+            130,
+            0.8
+        )
+        display_element(
+            self.screen,
+            './sprites/deco/deco_menu_2.png',
+            800, 
+            150,
+            0.3
+        )
+
     def display_menu(self, size, hoover_start, hoover_tutorial):
-        # Start box
+        # Title
+        deboo, debooRect = display_text(
+            './font/Atma-Bold.ttf', 
+            'DeBOO', 
+            48+50, 
+            self.Xscreen//2, 
+            self.Yscreen//2-100, 
+            (255,245,0), 
+            custom_font=True
+            # bold=True
+        )
+        deboo_shadow = display_textbox(
+            self.screen,
+            debooRect,
+            30, 50, 405, 155
+        )
+        deBoo_interact = display_textbox(
+            self.screen,
+            debooRect,
+            30, 50, 400, 150
+        )
+
+        # Start button
         if hoover_start:
             start, startRect = display_text(
                 './font/Atma-Bold.ttf', 
@@ -81,10 +105,8 @@ class Menu_frame():
                 startRect,
                 30, 50, 400, 100
             )
-        
-        pygame.draw.rect(self.screen, (255,245,0), startRect_interact_1, 0, 10)
-        self.screen.blit(start, startRect)
 
+        # Tutorial button
         if hoover_tutorial:
             tutorial, tutorialRect = display_text(
                 './font/Atma-Bold.ttf', 
@@ -127,40 +149,18 @@ class Menu_frame():
                 tutorialRect,
                 30, 50, 400, 100
             )
-
-        pygame.draw.rect(self.screen, (255,245,0), tutorialRect_shadow, 3, 10)
-        pygame.draw.rect(self.screen, (0,0,0), tutorialRect_interact_1, 0, 10)
-        self.screen.blit(tutorial, tutorialRect)
-
-        # Title
-        deboo, debooRect = display_text(
-            './font/Atma-Bold.ttf', 
-            'DeBOO', 
-            48+50, 
-            self.Xscreen//2, 
-            self.Yscreen//2-100, 
-            (255,245,0), 
-            custom_font=True
-            # bold=True
-        )
-        deboo_shadow = display_textbox(
-            self.screen,
-            debooRect,
-            30, 50, 405, 155
-        )
-        deBoo_interact = display_textbox(
-            self.screen,
-            debooRect,
-            30, 50, 400, 150
-        )
+        
         pygame.draw.rect(self.screen, (255, 254, 0), deboo_shadow, 3, 10)
         pygame.draw.rect(self.screen, (0,0,0), deBoo_interact, 0, 10)
-        self.screen.blit(deboo, debooRect)
+        pygame.draw.rect(self.screen, (255,245,0), startRect_interact_1, 0, 10)
+        pygame.draw.rect(self.screen, (255,245,0), tutorialRect_shadow, 3, 10)
+        pygame.draw.rect(self.screen, (0,0,0), tutorialRect_interact_1, 0, 10)
 
-        self.add_characters()
-        self.add_characters_2()
-        self.add_deco_1()
-        self.add_deco_2()
+        self.screen.blit(deboo, debooRect)
+        self.screen.blit(start, startRect)
+        self.screen.blit(tutorial, tutorialRect)
+
+        self.display_deco()
 
         return startRect, tutorialRect
 
